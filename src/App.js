586f9d0f6c60efa3e00883a35838778b6a58ad9e
Serja Aaltonen <serja.aaltonen@gmail.com>
2020-05-04 22:50:20 +0300

    Firebase database added

--- src/App.js
+++ src/App.js
@@ -1,8 +1,7 @@
 import React, {Component} from 'react';
 import { BrowserRouter as Router, Route} from 'react-router-dom';
 import './App.css'
-import testdata from './testdata';
-import firebase from './firebase';
+import firebase, {provider, auth}  from './firebase';
 
 import Header from './components/Header/Header';
 import Items from './components/Items/Items';
@@ -15,88 +14,112 @@ import Menu from './components/Menu/Menu';
 class App extends Component {
   
   constructor(props) {
+    
     super(props);
     this.state = {
-      data: testdata,
-      walletData : {
-        walletSaldo : 0.00,
-        meze :  0.00,
-        food : 0.00,
-        varioussmall : 0.00,
-        pharmacy : 0.00,
-        animals : 0.00,
-        gas : 0.0
-      }
+      data: [],
+      walletData : []     
     }
+
     this.dbRef = firebase.firestore();
     this.handleFormSubmit = this.handleFormSubmit.bind(this);
     this.setWalletandCat = this.setWalletandCat.bind(this);
+    
   }
 
   componentDidMount() {
     this.refData = this.dbRef.collection('data');
+    this.refData2 = this.dbRef.collection('walletData');
+
+    this.refData.orderBy("date", "desc").onSnapshot((docs) => {  //hakee datan DBsta ja jarjestaa sen uusin ensin
+      let data = [];
+      docs.forEach((doc) => {
+        let docdata = doc.data();
+        data.push(docdata);
+      });
+      this.setState({
+        data: data
+      });
+    });
+
+        
+    this.refData2.get().then(snapshot => {
+      snapshot.docs.forEach((doc) => {
+   
+        this.setState({
+          walletData: {
+              ...this.state.walletData,
+              [doc.id]: doc.data().total
+          }
+        });
+      });
+    });
+    
   }
 
+  
   handleFormSubmit(newdata) {
-    let storedata =  this.state.data.slice();
-    storedata.push(newdata);  //tallentaa uuden datan listaan
-
-    storedata.sort((a,b) => {   //jarjestaa listan uusin ensin jarjestykseen
-      const aDate = new Date(a.date);
-      const bDate = new Date(b.date);
-      return bDate.getTime() - aDate.getTime();     
-    });
-    this.setState({
-        data: storedata
-    });
-
-    this.refData.doc(newdata.id).set({newdata});  //tallentaa uuden tapahtuman Firebase-tietokantaan
+    
+    this.refData.doc(newdata.id).set(newdata);  //tallentaa uuden tapahtuman Firebase-tietokantaan
 
    }
 
-  setWalletandCat(catClass, catName, trsum) {
-    // funktio Walletin saldon muuttamiseksi ja maksuluokan saldojen muuttamiseksi. 
+ // funktio Walletin saldon muuttamiseksi ja maksuluokan saldojen muuttamiseksi. 
     // Saa parametreina Treansactionista 
         //catClass (onko tapatuman hyvaksynyt lompakko vai maksuluokka)
         //catName (maksuluokan nimi (jos on wallet, tuottaa wallet__img))
-        //trsum (siirrettava summa)
+        //sum (siirrettava summa)
 
     //jos catClass on maksuluokka, niin vahentaa summan lompakosta ja lisaa maksuluokkaan
     //muuten lisaa Transactionista tulevan summan lompakoon
+
+  setWalletandCat(catClass, catName, sum) {
     
-        
     let walletSaldo__old = parseFloat(this.state.walletData.walletSaldo);
     let catNameSaldo__old = parseFloat(this.state.walletData[catName]);
 
-   
-    if (catClass === "paymentcat__button" ) {
-      this.setState({ 
+    
+    if (catClass === "paymentcat__button" ) {       //onko tapahutma maksuluokka
+      
+      let total = parseFloat(catNameSaldo__old + sum);
+      this.refData2.doc(catName).update({total});  //paivittaa ko. maksuluokan total-kentan Firebase-tietokantaan
+
+      total =  parseFloat( walletSaldo__old - sum);
+      this.refData2.doc('walletSaldo').update({total});  //paivittaa lompakon uuden summan total-kenttaan Firebase-tietokantaan
+
+      this.setState({    
         walletData: {
-          ...this.state.walletData,
-          walletSaldo : parseFloat( walletSaldo__old - trsum),
-          [catName] : parseFloat(catNameSaldo__old + trsum)
+            ...this.state.walletData,
+            walletSaldo : parseFloat( total),
+            [catName]: parseFloat(catNameSaldo__old + sum)
         }
       });
+
     }
     else {
-      this.setState({ 
+      let total = parseFloat( walletSaldo__old + sum);
+      this.refData2.doc('walletSaldo').update({total});  //paivittaa lompakon uuden summan total-kenttaan Firebase-tietokantaan   
+      
+      this.setState({    
         walletData: {
-          ...this.state.walletData,
-          walletSaldo : parseFloat( walletSaldo__old = trsum)
+            ...this.state.walletData,
+            walletSaldo : parseFloat( total)            
         }
       });
     }
+        
   }
  
   render() {
 
     return (
-      <>
-        <Router>          
+         
+      <>      
+        <Router>       
           <div className="App" >   
           <Header />         
             <Route path="/" exact render={() => 
-              <Items onFormSubmit={this.handleFormSubmit} setWallet={this.setWalletandCat} walletData={this.state.walletData} />} />
+               <Items onFormSubmit={this.handleFormSubmit} setWallet={this.setWalletandCat} walletData={this.state.walletData}  />} />
               
             <Route path="/stats" render={() => <Stats data={this.state.data} />} />
             <Route path="/graph" component={Graph} />
